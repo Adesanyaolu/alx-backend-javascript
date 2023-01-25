b3e050a48f99f6697f38c226aad6dd6bb5dedde1
adesanyaolu <sanya4reel@gmail.com>
2023-01-25 15:06:29 +0000

    Task 0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,13 +1,13 @@
-const taskFirst = () => {
-    const task = 'I prefer const when I can.';
-    return task;
-  };
-  
-  const getLast = () => ' is okay';
-  
-  const taskNext = () => {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  };
+export const taskFirst = () => {
+  const task = 'I prefer const when I can.';
+  return task;
+};
+
+export const getLast = () => ' is okay';
+
+export const taskNext = () => {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+};
