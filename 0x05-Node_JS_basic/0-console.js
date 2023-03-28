447526d954de6fcaa99f1e9810aae86ec37f2001
adesanyaolu <sanya4reel@gmail.com>
2023-03-28 12:41:18 +0100

    All done

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,5 @@
-// import the displayMessage function from the 0-console.js file
 function displayMessage(data) {
-    console.log(data);
-  }
+  console.log(data);
+}
 
-// call the function to display a message
 module.exports = displayMessage;
